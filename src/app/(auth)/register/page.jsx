606040b5ed9f022906dbf599715f9dae68a90754
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2025-02-05 20:45:24 +0600

    form validation using zod

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,35 +1,59 @@
+"use client";
+
+import { register } from "@/actions/auth";
 import Link from "next/link";
+import { useActionState } from "react";
 
 export default function Register() {
+  const [state, action, isPending] = useActionState(register, undefined);
+  return (
+    <div className="container w-1/2">
+      <h1 className="title">Register</h1>
 
-    return (
-        <div className="container w-1/2">
-            <h1 className="title">Register</h1>
-            
-            <form action="" className="space-y-4">
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email"/>
-                </div>
-
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password"/>
-                </div>
-
-                <div>
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="text" name="confirmPassword"/>
-                </div>
-
-                <div className="flex items-end gap-4">
-                    <button className="btn-primary">Register</button>
-
-                    <Link href="/" className="text-link
-                    ">Or login here</Link>
-
-                </div>
-            </form>
+      <form action={action} className="space-y-4">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" defaultValue={state?.email}/>
+          {state?.errors?.email && (
+            <p className="error">{state.errors.email}</p>
+          )}
         </div>
-    );
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+          {state?.errors?.password && (
+            <div className="error">
+              <p>Password must:</p>
+              <ul className="list-disc list-inside ml-4">
+                {state.errors.password.map((err) => (
+                  <li key={err}>{err}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input type="text" name="confirmPassword" />
+          {state?.errors?.confirmPassword && <p className="error">{state.errors.confirmPassword}</p>}
+        </div>
+
+        <div className="flex items-end gap-4">
+          <button disabled={isPending} className="btn-primary">
+            {isPending ? "Loading.." : "Register"}
+          </button>
+
+          <Link
+            href="/"
+            className="text-link
+                    "
+          >
+            Or login here
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
